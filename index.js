8c39fcc90ef905ea6f2ce200d074c20685f5602f
Carmen <path4design@gmail.com>
2023-05-22 14:26:39 +0200

    update code

--- index.js
+++ index.js
@@ -1,8 +1,67 @@
+const usersArray = [
+  {
+    firstName: "Kirby",
+    lastName: "Doyle",
+    id: "b71794e5-851e-44b5-9eec-1dd4e897e3b8",
+    isActive: false,
+    balance: "$3,570.06",
+    gender: "male",
+  },
+  {
+    firstName: "Tracie",
+    lastName: "May",
+    id: "1af0e9ee-66fc-4298-b8ce-5d99bcbaac05",
+    isActive: false,
+    balance: "$1,547.73",
+    gender: "female",
+  },
+  {
+    firstName: "Kendra",
+    lastName: "Hines",
+    id: "5e92af3a-b08e-4689-bdeb-3226300470e3",
+    isActive: true,
+    balance: "$12,383.08",
+    gender: "female",
+  },
+  {
+    firstName: "Kinney",
+    lastName: "Howard",
+    id: "0ad2388d-83e1-4831-9cc4-e3581f8edf36",
+    isActive: false,
+    balance: "$3,207.06",
+    gender: "male",
+  },
+  {
+    firstName: "Howard",
+    lastName: "Gilmore",
+    id: "0719205d-c965-44cb-a128-708cf335b26c",
+    isActive: true,
+    balance: "$21,307.75",
+    gender: "male",
+  },
+  {
+    firstName: "Rachelle",
+    lastName: "Schneider",
+    id: "04012184-651b-41eb-9642-d362fedff02f",
+    isActive: true,
+    balance: "$35,121.49",
+    gender: "female",
+  },
+  {
+    firstName: "Lizzie",
+    lastName: "Alford",
+    id: "598ca7ec-888e-494d-ae94-c21ace3ffa52",
+    isActive: false,
+    balance: "$4,382.94",
+    gender: "female",
+  },
+];
+
 // ***************************************************************************
 // Iteration 1 - `for...of` loop
 // ***************************************************************************
 
-const getFirstNames = arr => {
+const getFirstNames = (arr) => {
   const userFirstNames = [];
   for (let user of arr) {
     // Your code goes here ...
@@ -17,7 +76,7 @@ getFirstNames(usersArray);
 // Iteration 2 - `for...of` loop and ES6 string literals `${}`
 // ***************************************************************************
 
-const getFullNames = arr => {
+const getFullNames = (arr) => {
   // Your code goes here ...
 };
 
@@ -30,7 +89,7 @@ getFullNames(usersArray);
 // Iteration 3 - ES6 destructuring , for of loop, object literal
 // ***************************************************************************
 
-const getUsersCreditDetails = arr => {
+const getUsersCreditDetails = (arr) => {
   // Your code goes here ...
 };
 
@@ -48,11 +107,23 @@ getUsersCreditDetails(usersArray);
 // Iteration 4 - practice `.filter()` method and how to return two elements
 // ***************************************************************************
 
-const genderView = users => {
-  // Your code goes here ...
+const genderView = (users) => {
+  // 1. Hay que usar el método filter dos veces. ¿Que propiedad de cada obeto vamos a usar para filtrar el array? gender
+
+  let femaleUsersObjects = users.filter(user => user.gender === 'female'); // = usar adecuadamente el metodo filter sobre 'users' para que me devuelva solo las mujeres
+  
+  let maleUsersObjects = users.filter(user => user.gender === 'male'); // = usar adecuadamente el metodo filter sobre 'users' para que me devuelva solo los hombres
+
+  // 2. Hay que devolver un objeto con dos propiedades.
+  // 2.1 La propiedad femaleUsers contiene el nombre de todas las mujeres. Usar el método map
+  return {
+    femaleUsers: femaleUsersObjects.map(female => `${female.firstName} ${female.lastName}`),
+    maleUsers: maleUsersObjects.map(male => `${male.firstName} ${male.lastName}` )
+  }
+  // 2.2 LA propiedad maleUSres contiene el nombre de todos los hombres. USar el método map
 };
 
-genderView(usersArray);
+console.log("gender view", genderView(usersArray));
 // expected output:
 // {
 //    femaleUsers: [ 'Tracie May', 'Kendra Hines', 'Rachelle Schneider', 'Lizzie Alford' ],
@@ -65,7 +136,7 @@ genderView(usersArray);
 
 const data = genderView(usersArray);
 
-const genderCount = data => {
+const genderCount = (data) => {
   // Your code goes here ...
 };
 
@@ -78,7 +149,7 @@ genderCount(data);
 // Bonus - Iteration 6
 // ***************************************************************************
 
-const promo20 = users => {
+const promo20 = (users) => {
   // Your code goes here ...
 };
 
@@ -90,7 +161,7 @@ const promo20 = users => {
 // Bonus - Iteration 7
 // ***************************************************************************
 
-const addActive = users => {
+const addActive = (users) => {
   // Your code goes here ...
 };
 
